ffe6717fbade4d319424c11a29899af72490ba28
QoidynBasy <c0mplexie1@gmail.com>
2023-04-04 08:16:27 +0600

    problem10

---
+++ GCD.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class GCD {
+    public static int gcd(int a, int b)
+    {
+        int c;
+        if (a == b)
+        {
+            return a;
+        }
+        if (a == 1 || b == 1)
+        {
+            return 1;
+        }
+        if (a == 0 || b == 0)
+        {
+            return 0;
+        }
+        int y;
+        y = Math.min(a, b);
+        c = y;
+        while(a % b != 0)
+        {
+            c = a % b;
+            a = b;
+            b = c;
+        }
+        return c;
+
+    }
+    public static void main(String[] args)
+    {
+        Scanner number = new Scanner(System.in);
+        int a = number.nextInt();
+        int b = number.nextInt();
+        System.out.println(gcd(a,b));
+    }
+}
