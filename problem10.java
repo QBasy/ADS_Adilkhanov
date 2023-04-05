701a78225eb7e2bdf44869862bbb41f4f0348d51
QBasy <124850927+QBasy@users.noreply.github.com>
2023-04-05 13:50:51 +0600

    Update problem10.java

--- problem10.java
+++ problem10.java
@@ -1,38 +1,36 @@
 import java.util.Scanner;
 
 public class Main {
-    public static int GCD(int a, int b)
+    public static int FindGCD(int a, int b) // Creating function to find GCD of user values a and b
     {
-        int c;
-        if (a == b)
+        int c; // Creating variable c without value
+        if (a == b) // First statement with condition a is equal b
         {
-            return a;
+            return a; // If statement true it will return function with value of given numbers
         }
-        if (a == 1 || b == 1)
+        if (a == 1 || b == 1) // Second statement checking is any value are equal to 1
         {
-            return 1;
+            return 1; // If statement true it will return function with value 1
         }
-        if (a == 0 || b == 0)
+        if (a == 0 || b == 0) // Third statement checking is any value are equal to 0
         {
-            return 0;
+            return 0; // If statement true it will return function with value 0
         }
-        int y;
-        y = Math.min(a, b);
-        c = y;
-        while(a % b != 0)
+        c = Math.min(a, b); // Giving variable c the minimal value of the a or b
+        while(a % b != 0) // Creating the loop to find out the greatest common divisor
         {
             c = a % b;
             a = b;
             b = c;
         }
-        return c;
+        return c; // Returns the GCD of a and b
 
     }
     public static void main(String[] args)
     {
-        Scanner number = new Scanner(System.in);
-        int a = number.nextInt();
-        int b = number.nextInt();
-        System.out.println(GCD(a,b));
+        Scanner number = new Scanner(System.in); // Creating Scanner function to read user input values
+        int a = number.nextInt(); // Creating and giving variable a user values
+        int b = number.nextInt(); // Creating and giving variable b user values
+        System.out.println(FindGCD(a,b)); // Output the GCD of a and b by using function
     }
 }
