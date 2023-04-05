b366607453173811cc9e1707efed85436fc6cb68
QBasy <124850927+QBasy@users.noreply.github.com>
2023-04-05 13:19:33 +0600

    Update problem5.java

--- problem5.java
+++ problem5.java
@@ -1,17 +1,18 @@
 import java.util.Scanner;
 public class Main {
-    public static int Fibonacci(int n)
+    public static int Fibonacci(int n) // Creating function to find Fibonacci
     {
-        if (n == 0)(n == 1) {
-        return n;
-    } else {
-        return Fibonacci(n - 1) + Fibonacci(n - 2);
-    }
+        if (n == 0 || n == 1) // Creating if statement with conditions  n=1 and n=0
+        {
+            return n; // if the conditions are true it will return given number
+        } else {
+            return Fibonacci(n - 1) + Fibonacci(n - 2); // Returns the Fibonacci solution by using recursive function
+        }
     }
     public static void main(String[] args)
     {
-        Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
-        System.out.println(Fibonacci(n));
+        Scanner scanner = new Scanner(System.in); // Creating Scanner function to read user input values
+        int n = scanner.nextInt(); // Creating varaiable n and giving the user input value
+        System.out.println(Fibonacci(n)); // Output founded Fibonacci by the using function with user value
     }
 }
