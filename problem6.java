4a30ff350fe614fb78d55a2806203db7ddc253e4
QBasy <124850927+QBasy@users.noreply.github.com>
2023-04-05 13:24:38 +0600

    Update problem6.java

--- problem6.java
+++ problem6.java
@@ -2,15 +2,15 @@ import java.util.Scanner;
 import static java.lang.Math.pow;
 
 public class Main {
-    public static double Power(double n, double a)
+    public static double Power(double n, double a) // Creating function to find value a in power n
     {
-        return pow(a,n);
+        return pow(a,n); // Returns value a in power n by Using java.lang.Math.pow
     }
     public static void main(String[] args)
     {
-        Scanner num = new Scanner(System.in);
-        double a = num.nextDouble();
-        double n = num.nextDouble();
-        System.out.println(Power(n,a));
+        Scanner num = new Scanner(System.in); // Creating Scanner function to read user input values
+        double a = num.nextDouble(); // Creating the first variable to give a number value by using scanner
+        double n = num.nextDouble(); // Creating the second value to give a power value by using scanner
+        System.out.println(Power(n,a)); // Outputs the value of the number a in power of n by using function Power
     }
 }
