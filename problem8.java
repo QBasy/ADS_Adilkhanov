2fc3db5632d6d63b29f1e1a43625b9fe8ffd53e6
QBasy <124850927+QBasy@users.noreply.github.com>
2023-04-05 13:37:52 +0600

    Update problem8.java

--- problem8.java
+++ problem8.java
@@ -1,23 +1,23 @@
 import java.util.Scanner;
 
 public class Main {
-    public static String Checker(String a)
+    public static String IsVariableContainsLetter(String a) // Creating function to find out if our variable contains any letter
     {
-        char c;
-        for (c = 'a'; c <= 'z'; c++)
+        char c; // Creating char variable without values
+        for (c = 'a'; c <= 'z'; c++) // Creating loop that gives our char values of every letter
         {
-            if (a.contains(String.valueOf(c)))
+            if (a.contains(String.valueOf(c))) // This statement checking out is our variable contains any letter
             {
-                return "No";
+                return "No"; // If any letter is inside of the variable it returns function with value "No"
             }
 
         }
-        return "Yes";
+        return "Yes"; // If variable doesn't contains any letter function returns with value "Yes"
     }
     public static void main(String[] args)
     {
-        Scanner scanner = new Scanner(System.in);
-        String a = scanner.next();
-        System.out.println(Checker(a));
+        Scanner scanner = new Scanner(System.in); // Creating Scanner function to read user input
+        String a = scanner.next(); // Creating variable with user value
+        System.out.println(IsVariableContainsLetter(a)); // Outputs the final solution of the function about our variable
     }
 }
