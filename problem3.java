15d839cfd89f61fdc761888f344a5a5dacbd5054
QBasy <124850927+QBasy@users.noreply.github.com>
2023-04-05 13:05:59 +0600

    Update problem3.java

--- problem3.java
+++ problem3.java
@@ -1,20 +1,21 @@
 import java.util.Scanner;
 public class Main {
-    public static String primefinder(int n)
+    public static String FindPrimeNumber(int n) // Creating function to find Prime number
     {
-        String a = "Prime";
-        for (int i = 2; i <= n / 2; ++i)
+        String a = "Prime"; // Creating String variable with the value "Prime"
+        for (int i = 2; i <= n / 2; ++i) // Creating Loop through all numbers from 2 to n/2
         {
-            if (n % i == 0) {
-                a = "Composite";
+            if (n % i == 0) // Creating if statement to find our number is prime or not
+            {
+                a = "Composite"; // If our number isn't prime our variable takes the value "Composite"
             }
         }
-        return a;
+        return a; // Returns the final value of the variable a
     }
     public static void main(String[] args)
     {
-        Scanner number = new Scanner(System.in);
-        int n = number.nextInt();
-        System.out.println(primefinder(n));
+        Scanner number = new Scanner(System.in); // Creating function Scannet to read user input
+        int n = number.nextInt(); // Creating variable n and reading the user input
+        System.out.println(FindPrimeNumber(n)); // Outputs the final solution of the function
     }
 }
