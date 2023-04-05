89ae24a5af274a36c1a0ea81f186fdb1865bea69
QBasy <124850927+QBasy@users.noreply.github.com>
2023-04-05 12:53:38 +0600

    Update problem2.java

--- problem2.java
+++ problem2.java
@@ -1,23 +1,24 @@
 import java.util.Scanner;
 public class Main {
-    public static double avg(int[] arr, int n)
+    public static double FindAverageOfArray(int[] arr, int n) // Creating a function to find the average value inside the array
     {
-        double avg = 0;
-        for (int j : arr) {
-            avg = avg + j;
+        double avg = 0; // Initialize a variable average
+        for (int j : arr) // Creating loop to summarize every value of array to our average
+        {
+            avg = avg + j; // Here is the summarizing
         }
-        avg = avg/n;
-        return avg;
+        avg = avg/n; // Dividing our average to array size
+        return avg; // Returning completed average
     }
     public static void main(String[] args)
     {
-        Scanner num = new Scanner(System.in);
-        int n = num.nextInt();
-        int[] arr = new int[n];
-        for (int i = 0; i < n; i++)
+        Scanner num = new Scanner(System.in); // Creating Scanner function to read user input
+        int n = num.nextInt(); // Input size of array
+        int[] arr = new int[n]; // Creating the array with given size
+        for (int i = 0; i < n; i++) // Creating loop to input each value of array
         {
-            arr[i] = num.nextInt();
+            arr[i] = num.nextInt(); // There is input
         }
-        System.out.println(avg(arr,n));
+        System.out.println(FindAverageOfArray(arr,n)); // Output average by using function
     }
 }
