2c811b461c5ef4f4fb4b7deb494b69a95c479c80
QBasy <124850927+QBasy@users.noreply.github.com>
2023-04-05 12:43:12 +0600

    Update problem1.java

--- problem1.java
+++ problem1.java
@@ -1,32 +1,32 @@
 import java.util.Scanner;
 
 public class Main {
-    public static int smallest(int[] arr, int n)
+    public static int FindSmallestValueInArray(int[] arr, int n) // Creating a function to find the smallest value in array
     {
-        int lowest = 0;
-        for (int i = 0; i < n; i++)
+        int lowest = 0; // Initialize variable to hold the smallest value found
+        for (int i = 0; i < n; i++) // This loop iterates through each element of the array
         {
-            for (int j = i + 1; j < n; j++)
+            for (int j = i + 1; j < n; j++) // This loop iterates through all elements after the current element
             {
-                if (arr[i] > arr[j])
+                if (arr[i] > arr[j]) // Compare the current element with each of the elements after it
                 {
-                    lowest = arr[i];
-                    arr[i] = arr[j];
-                    arr[j] = lowest;
+                    lowest = arr[i]; // Save the element as the smallest found so far
+                    arr[i] = arr[j]; // Swap the element with the smaller
+                    arr[j] = lowest; // Set the smaller element as the new current element
                 }
             }
         }
-        return arr[0];
+        return arr[0]; // Returning the smallest value of given array
     }
     public static void main(String[] args)
     {
-        Scanner number = new Scanner(System.in);
-        int n = number.nextInt();
-        int[] arr = new int[n];
-        for (int i = 0; i < n; i++)
+        Scanner scanner = new Scanner(System.in); // Creating Scanner to read user input
+        int n = scanner.nextInt(); // Input array size
+        int[] arr = new int[n]; // Creating the array with our array size
+        for (int i = 0; i < n; i++) // Creating loop to add each element off array by the Scanner
         {
-            arr[i] = number.nextInt();
+            arr[i] = scanner.nextInt(); // Input each element of array
         }
-        System.out.println(smallest(arr,n));
+        System.out.println(FindSmallestValueInArray(arr,n)); // Output the smallest value of array by using function
     }
 }
