310798301f4de6eb93d4f2d2ee3665592a183eb0
QBasy <124850927+QBasy@users.noreply.github.com>
2023-04-05 13:41:48 +0600

    Update problem9.java

--- problem9.java
+++ problem9.java
@@ -1,22 +1,22 @@
 import java.util.Scanner;
 
 public class Main {
-    public static int binomialCoefficient(int n, int k)
+    public static int FindBinomialCoefficient(int n, int k) // Creating the function to find binomial coefficent by using user values n and k
     {
-        if (k == 0 || n == k)
+        if (k == 0 || n == k) // Creating statement to check if k is equal 0, or is n equal k
         {
-            return 1;
+            return 1; // Returns 1 if statement is true
         }
         else {
-            return binomialCoefficient(n-1, k-1) + binomialCoefficient(n-1, k);
+            return FindBinomialCoefficient(n-1, k-1) + FindBinomialCoefficient(n-1, k); // Returns the final solution by using the recursive function
         }
     }
 
     public static void main(String[] args)
     {
-        Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
-        int k = scanner.nextInt();
-        System.out.println(binomialCoefficient(n,k));
+        Scanner scanner = new Scanner(System.in); // Creating Scanner function to read user input
+        int n = scanner.nextInt(); // Creating and giving value to variable n
+        int k = scanner.nextInt(); // Creating and giving value to variable k
+        System.out.println(FindBinomialCoefficient(n,k)); // Output the final solution of function by using user values of n and k
     }
 }
