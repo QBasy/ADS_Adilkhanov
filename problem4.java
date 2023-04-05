827d920ab9627e941ca4563e9eb256beffd4332c
QBasy <124850927+QBasy@users.noreply.github.com>
2023-04-05 13:11:18 +0600

    Update problem4.java

--- problem4.java
+++ problem4.java
@@ -1,19 +1,19 @@
 import java.util.Scanner;
 
 public class Main {
-    public static int factorial(int n)
+    public static int Factorial(int n) // Creating function to find factorial solution
     {
-        int avg = 1;
-        for (int j = 1; j <= n; j++)
+        int avg = 1; // Creating variable avg with value 1
+        for (int j = 1; j <= n; j++) // Creating loop to multiply our avg to all values from 1 to n
         {
-            avg = avg * j;
+            avg = avg * j; // Multiplying avg to value
         }
-        return avg;
+        return avg; // Returns the solution of the factorial
     }
     public static void main(String[] args)
     {
-        Scanner num = new Scanner(System.in);
-        int n = num.nextInt();
-        System.out.println(factorial(n));
+        Scanner num = new Scanner(System.in); // Creating Scanner function to read user input
+        int n = num.nextInt(); // Creating variable n and giving it user value
+        System.out.println(Factorial(n)); // Outputs the solution of function
     }
 }
